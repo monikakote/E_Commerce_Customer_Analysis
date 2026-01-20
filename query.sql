0e916cf11ef4c636db53bc824dda2e6e2e62d27d
Monika_kote20 <monikakote35@gmail.com>
2026-01-20 20:36:45 +0530

    Create query.sql for SQL analysis documentation
    Add SQL analysis documentation with key queries listed.

---
+++ query.sql
@@ -0,0 +1,10 @@
+
+🧮 SQL Analysis
+--
+All PostgreSQL queries used for analysis are available in the `sql/` folder.
+--
+Key queries include:
+- Category-wise revenue analysis
+- Payment method distribution
+- Repeat customer identification
+- Discount impact analysis
